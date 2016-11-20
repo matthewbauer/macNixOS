8866e2bcc98f38b09d284ff8e7e24e9e3e6204d1
Matthew Bauer <mjbauer95@gmail.com>
2016-11-20 17:21:57 -0600

    Add link-apps.sh

---
+++ links-apps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+NIX_PROFILE="$HOME"/.nix-profile
+APP_DIR="$HOME"/Applications
+
+for f in "$NIX_PROFILE"/Applications/*; do
+    app_name="$(basename "$f")"
+    if [ ! -d "$APP_DIR/$app_name" ]; then
+        ln -s "$f" "$APP_DIR"/
+    fi
+done
