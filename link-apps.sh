14fa1ff390a0fe620eda4c44cffcc2a5f953f753
Matthew Bauer <mjbauer95@gmail.com>
2016-11-25 00:57:21 -0600

    Add remove old links
    This will get rid of old links to NIX_PROFILE.

--- link-apps.sh
+++ link-apps.sh
@@ -3,6 +3,17 @@
 NIX_PROFILE="$HOME"/.nix-profile
 APP_DIR="$HOME"/Applications
 
+# remove old links
+for f in "$APP_DIR"/*; do
+    link="$(readlink $f)"
+    if [ ! -z "$link" ]; then
+        if [[ "$link" == "$NIX_PROFILE/Applications/"* ]]; then
+            rm "$f"
+        fi
+    fi
+done
+
+# link new ones
 for f in "$NIX_PROFILE"/Applications/*; do
     app_name="$(basename "$f")"
     if [ ! -e "$APP_DIR/$app_name" ]; then
